12994ac28a10c8c28c8cee7938144c1ea35fae91
Mohsen Ghanbari <ghanbarii@aol.com>
2022-12-20 14:25:22 +0330

    feat: new design

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Navigation />
+    <div class="header mt-20">
+      <div class="header__person float-left md:ml-60 ml-0">
+        <img src="../assets/img/first page/png/character.png" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+@import '@/assets/css/fonts.css';
+
+.header__person{
+  width: 464px;
+  height: 884px;
+}
+
+</style>
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
